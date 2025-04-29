29ccc7fa64a0387463d3f2c50ae70c9fbb43fb72
somnicattus <somnicattus@gmail.com>
2025-04-29 18:22:10 +0900

    Adds minify option to build script
    Introduces a command-line flag (`--minify` or `-m`) to enable minification during the build process.
    
    Refactors path handling using constants and `path.join` for improved clarity and maintainability.

--- build.ts
+++ build.ts
@@ -1,16 +1,24 @@
+import { argv } from 'bun';
 import { readdir } from 'node:fs/promises';
+import path from 'node:path';
+
+const [_exe, _file, flag] = argv;
+const minify = flag === '--minify' || flag === '-m';
+
+const source = './scripts';
+const destination = './build';
 
 // Cleanup
-const oldFiles = await readdir('./build');
+const oldFiles = await readdir(destination);
 for (const file of oldFiles) {
-  await Bun.file(`./build/${file}`).delete();
+  await Bun.file(path.join(destination, file)).delete();
 }
 
 // Build
-const files = await readdir('./scripts');
-const entrypoints = files.map((file) => `./scripts/${file}`);
+const files = await readdir(source);
+const entrypoints = files.map((file) => path.join(source, file));
 await Promise.all(
   entrypoints.map((entrypoint) =>
-    Bun.build({ entrypoints: [entrypoint], outdir: './build' }),
+    Bun.build({ entrypoints: [entrypoint], outdir: destination, minify }),
   ),
 );
